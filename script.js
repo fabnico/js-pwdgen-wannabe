7568642af1d852f2c47f55033443dd62fa739790
Fabrizio <whoet.wo@gmail.com>
2020-10-19 16:04:07 +0200

    Esercizio completato

---
+++ script.js
@@ -0,0 +1,4 @@
+var nomeutente = prompt('Inserisci il tuo nome');
+var cognomeutente = prompt('Inserisci il tuo cognome');
+var colorepreferito = prompt('Qual è il tuo colore preferito?');
+document.getElementById('my_pwdgen').innerHTML = nomeutente + cognomeutente + colorepreferito + ('19');
